b35f3ef18851473af645d3b45a1abc02f5065a0c
Rúnar Berg <runarberg@zoho.com>
2023-10-10 16:50:05 -0700

    draw basic shapes to canvas
    solves #2

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 async function main() {
-  const canvas = document.getElementById("canvas");
+  const canvas = document.getElementById("game");
 
   if (!(canvas instanceof HTMLCanvasElement)) {
     return;
   }
 
   const ctx = canvas.getContext("2d");
+
+  // Clear the canvas with a white background
+  ctx.fillStyle = "white";
+  ctx.rect(0, 0, 500, 500);
+  ctx.fill();
+
+  // Ball
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  ctx.arc(250, 250, 7.5, 0, 2 * Math.PI);
+  ctx.fill();
+
+  // Paddle 1
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  ctx.rect(100, 20, 80, 15);
+  ctx.fill();
+
+  // Paddle 2
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  ctx.rect(320, 465, 80, 15);
+  ctx.fill();
 }
 
 document.addEventListener("DOMContentLoaded", main);
